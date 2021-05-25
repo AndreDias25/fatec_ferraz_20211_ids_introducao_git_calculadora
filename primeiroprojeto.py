379f32dba502bd5f259f2b9c0a5b961df54b9bdb
André <andre25dias@gmail.com>
2021-05-25 09:41:05 -0300

    versão com exponenciação!

--- primeiroprojeto.py
+++ primeiroprojeto.py
@@ -1,9 +1,19 @@
 A = int(input("Digite o 1º valor: "))
 B = int(input("Digite o 2º valor: "))
-operacao = input("Digite Soma(soma) ou Subtração(sub):")
+operacao = input("Digite Soma(soma), Subtração(sub), Multiplicação(mult), Divisão(div) ou Exponenciação(exp):")
 if operacao == "soma":
     Soma = A + B
-    print("Resultado da soma :", Soma)
+    print("Resultado da subtração :", Soma)
 elif operacao == "sub":
-    Produto = A*B
-    print("Produto :", Produto) 
+    sub = A-B
+    print("Subtração :", sub)
+elif operacao == "mult":
+    mult = A * B
+    print("Multiplicação: ", mult)
+elif operacao == "div":
+    div = A/B
+    print("Divisão: ", div)
+elif operacao == "exp":
+    exp = A ** B
+    print("Exponenciação: ", exp) 
+print("Operação realizada!!!")  
